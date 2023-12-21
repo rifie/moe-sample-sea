37edec15486b6796932c2548dd805f0f7bef22f7
Syarifah Riefandania <rifiesy@yahoo.com>
2023-12-21 13:49:19 +0700

    add img front

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button, Input, Modal } from 'flowbite-svelte';
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button, Input, Modal, Heading } from 'flowbite-svelte';
     import { onMount } from 'svelte';
 
     import { Alert } from 'flowbite-svelte';
@@ -11,7 +11,11 @@
   
   </script>
   
+<style>
+  .main_cont { margin :  80px auto; max-width: 880px;}
 
+  .front-image { margin : auto; background-color: #c8c8c8; height : 300px;}
+</style>
 
   <Header />
 
@@ -20,5 +24,16 @@
 
 
     <div class="main_cont">
+
+      <!-- image banner -->
+
+      <div class="front-image">
+
+        <Heading tag="h4" class="text-center pt-2">Image here for all users</Heading>
+        <img src="https://www.rifie.com/images/calm-sea.jpg" />
+
+
+
+      </div>
 
     </div>
